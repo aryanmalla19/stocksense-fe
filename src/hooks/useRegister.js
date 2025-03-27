eed1c15e7d5de899d3929e82dfebae9cf897014f
Raineetu <raineetu0070@gmail.com>
2025-03-27 14:24:43 +0545

    feat(api): Add Axios API service for user registration and login

---
+++ src/hooks/useRegister.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { register } from "../apiService/ApiService";
+
+const useRegister = () => {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await register({ input });
+      console.log(response);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { input, setInput, error, loading, handleSubmit };
+};
+
+export default useRegister;
--- src/pages/RegisterPages.jsx
+++ src/pages/RegisterPages.jsx
@@ -3,30 +3,63 @@ import { Link } from "react-router-dom";
 import SocialLogin from "../auth/SocialLogin";
 import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 import RegisterHeader from "../auth/RegisterHeader";
+import useRegister from "../hooks/useRegister";
 
 const RegisterPages = () => {
+  const { input, setInput, error, loading, handleSubmit } = useRegister();
+
+  // General onChange handler for all input fields
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="auth-bg">
       <div className="auth-container">
         <RegisterHeader />
         {/* Login form */}
-        <form className="space-y-5 mx-10 my-4">
+        <form className="space-y-5 mx-10 my-4" onSubmit={handleSubmit}>
           {/* Username input field */}
           <div className="relative">
             <FaUser className="auth-icon" />
-            <input type="text" placeholder="Username" className="auth-input" />
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              className="auth-input"
+              value={input.username}
+              onChange={handleInputChange}
+            />
           </div>
 
           {/* Email input field */}
           <div className="relative">
             <FaEnvelope className="auth-icon" />
-            <input type="text" placeholder="Email" className="auth-input" />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="auth-input"
+              value={input.email}
+              onChange={handleInputChange}
+            />
           </div>
 
           {/* Password input field */}
           <div className="relative">
             <FaLock className="auth-icon" />
-            <input type="text" placeholder="Password" className="auth-input" />
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              className="auth-input"
+              value={input.password}
+              onChange={handleInputChange}
+            />
           </div>
 
           {/* Submit button */}
