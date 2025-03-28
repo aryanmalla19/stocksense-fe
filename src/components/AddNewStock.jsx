ee75048adfa12b7a67bf9e81dfb7ff9f8ff734aa
Raineetu <raineetu0070@gmail.com>
2025-03-28 10:10:09 +0545

    feat(AddNewStock): Added add icon

--- src/components/AddNewStock.jsx
+++ src/components/AddNewStock.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "./Button";
+import { IoIosAddCircle } from "react-icons/io";
 
 const AddNewStock = () => {
   return (
     <div>
       Add New Stock
-      <Button classes="bg-teal-500 hover:bg-teal-700">Add</Button>
+      <Button classes="bg-teal-500 hover:bg-teal-700 flex gap-2 w-[500px] xl:w-auto items-center">
+        <IoIosAddCircle />
+        Add
+      </Button>
     </div>
   );
 };
