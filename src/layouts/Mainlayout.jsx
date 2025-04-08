dedcb55828ba8f3a89e86513389726cf21b7e495
Raineetu <raineetu0070@gmail.com>
2025-04-08 12:02:26 +0545

    Added protected Routes

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -3,26 +3,29 @@ import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
+import PrivateRoute from "../components/common/PrivateRoute";
 
 const Mainlayout = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <div className="flex fixed w-full">
-      <div className="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
-        <Sidebar />
-      </div>
-      <div className="bg-[#f7f8fa] flex-1">
-        <Navbar />
-        <div
-          className={`p-6 h-full ${
-            theme === "dark"
-              ? "bg-[var(--dark-bg-primary)] text-[var(--dark-text-primary)] border-r border-[var(--dark-border-primary)]"
-              : "bg-[--bg-secondary)] text-[var(--text-primary)]"
-          }`}
-        >
-          <Outlet />
+      <PrivateRoute>
+        <div className="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          <Sidebar />
         </div>
-      </div>
+        <div className="bg-[#f7f8fa] flex-1">
+          <Navbar />
+          <div
+            className={`p-6 h-full ${
+              theme === "dark"
+                ? "bg-[var(--dark-bg-primary)] text-[var(--dark-text-primary)] border-r border-[var(--dark-border-primary)]"
+                : "bg-[--bg-secondary)] text-[var(--text-primary)]"
+            }`}
+          >
+            <Outlet />
+          </div>
+        </div>
+      </PrivateRoute>
     </div>
   );
 };
