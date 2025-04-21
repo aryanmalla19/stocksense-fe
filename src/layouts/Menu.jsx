a996aafbecf5b357818952d011c4372ce3f15e42
Aryan Malla <aryanmalla19@gmail.com>
2025-04-21 11:05:52 +0545

    fixed Menu

--- src/layouts/Menu.jsx
+++ src/layouts/Menu.jsx
@@ -7,27 +7,16 @@ import {
   FiPieChart,
   FiLayers,
   FiShoppingCart,
-  FiTrendingUp,
 } from "react-icons/fi";
 import { IoMdHelpCircleOutline } from "react-icons/io";
 
 const menuItems = [
-  {
-    title: "Market Open",
-    items: [
-      { icon: <FiTrendingUp />, label: "Dashboard", href: "/" },
-      { icon: <FiList />, label: "Stocks List", href: "/stocks" },
-      { icon: <FiShoppingCart />, label: "Trade Stocks", href: "/buysell" },
-      { icon: <FiStar />, label: "Watchlist", href: "/watch-list" },
-    ],
-  },
-  {
-    title: "PORTFOLIO",
-    items: [
-      { icon: <FiPieChart />, label: "Portfolio", href: "/portfolio" },
-      { icon: <FiLayers />, label: "IPO Applications", href: "/shares" },
-    ],
-  },
+  { icon: <MdOutlineDashboard />, label: "Dashboard", href: "/" },
+  { icon: <FiList />, label: "Stocks List", href: "/stocks" },
+  { icon: <FiShoppingCart />, label: "Trade Stocks", href: "/buysell" },
+  { icon: <FiStar />, label: "Watchlist", href: "/watch-list" },
+  { icon: <FiPieChart />, label: "Portfolio", href: "/portfolio" },
+  { icon: <FiLayers />, label: "IPO Applications", href: "/shares" },
 ];
 
 const Menu = ({ collapsed, theme }) => {
@@ -41,31 +30,34 @@ const Menu = ({ collapsed, theme }) => {
       }`}
     >
       <div className="flex-1 overflow-y-auto py-10">
-        {menuItems.map((category) => (
-          <div className="mb-6" key={category.title}>
-            {!collapsed && (
-              <button className="hidden md:inline px-6 py-2 text-[15px] font-semibold uppercase tracking-wider bg-teal-700 hover:bg-teal-800 text-white rounded-md ml-5">
-                {category.title}
-              </button>
-            )}
-            <div className="mt-3">
-              {category.items.map((item) => (
-                <Link
-                  to={item.href}
-                  key={item.label}
-                  className={`flex items-center px-6 py-3 text-[16px] font-medium transition-colors duration-200 mx-3 rounded-md ${
-                    theme === "dark" ? "hover:bg-gray-700" : "hover:bg-gray-200"
-                  }`}
-                >
-                  <span className="mr-3 text-lg">{item.icon}</span>
-                  {!collapsed && (
-                    <span className="hidden md:inline">{item.label}</span>
-                  )}
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
+        <div className="mt-3">
+          {menuItems.map((item) => {
+            const isActive = currentPath === item.href;
+            return (
+              <Link
+                to={item.href}
+                key={item.label}
+                className={`flex items-center px-6 py-3 text-[16px] font-medium transition-colors duration-200 mx-3 rounded-md 
+                ${
+                  isActive
+                    ? theme === "dark"
+                      ? "bg-button-bg"
+                      : "bg-gray-200"
+                    : ""
+                } 
+                ${
+                  theme === "dark" ? "hover:font-semibold" : "hover:bg-gray-100"
+                }`}
+              >
+                <span className="mr-3 text-lg">{item.icon}</span>
+                {!collapsed && (
+                  <span className="hidden md:inline">{item.label}</span>
+                )}
+              </Link>
+            );
+          })}
+        </div>
+
         <div className="mt-40">
           <hr
             className={`border ${
