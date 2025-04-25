908609e5c50a8483d04a8cf81c41a84dabb124f1
Sandip Magar <reone054@gmail.com>
2025-04-25 14:23:01 +0545

    api to mark all notification as read

--- src/api/notificationApi.js
+++ src/api/notificationApi.js
@@ -12,3 +12,16 @@ export const fetchNotifications = async () => {
     throw error;
   }
 };
+
+export const markAllAsRead = async () => {
+  try {
+    const response = await axiosInstance.get("/users/markasread-notifications");
+    return response.data?.data;
+  } catch (error) {
+    console.log(
+      "Error marking user notification as read :",
+      error.response?.data || error
+    );
+    throw error;
+  }
+};
