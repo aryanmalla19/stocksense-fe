27eae84975e3f61eea3bf419936f779ceb7d6113
Raineetu <raineetu0070@gmail.com>
2025-04-28 23:03:20 +0545

    Updated the IPO list

--- src/pages/sharePage/IPOList.jsx
+++ src/pages/sharePage/IPOList.jsx
@@ -28,8 +28,9 @@ const IPOList = () => {
         }`}
       >
         <div className="overflow-x-auto">
-          <div className="grid grid-cols-4 bg-purple-button  text-white font-semibold p-2 rounded-md">
+          <div className="grid grid-cols-5 bg-purple-button  text-white font-semibold p-2 rounded-md">
             <p>Applied Date</p>
+            <p>Applied Time</p>
             <p>Applied Shares</p>
             <p>Allotted Shares</p>
             <p>Status</p>
@@ -43,20 +44,30 @@ const IPOList = () => {
             ipoApplication.map((item, index) => {
               return (
                 <div
-                  className={`grid grid-cols-4 py-4 text-sm border-b hover:bg-gray-100/10 ${
+                  className={`grid grid-cols-5 px-2 py-3 text-sm rounded-md hover:bg-gray-100/10 ${
                     theme === "dark"
                       ? "hover:bg-gray-700/30"
                       : "hover:bg-gray-100"
                   }`}
                   key={index}
                 >
-                  <p className="flex items-center">{item.applied_date}</p>
+                  <p className="flex items-center">
+                    {new Date(item.applied_date).toISOString().split("T")[0]}
+                  </p>
+                  <p className="flex items-center">
+                    {new Date(item.applied_date).toLocaleTimeString([ ], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                      hour12: true,
+                    })}
+                  </p>
                   <p className="flex items-center">{item.applied_shares}</p>
                   <p className="flex items-center">{item.allotted_shares}</p>
                   <p
                     className={`flex items-center justify-center p-2 rounded-md text-sm w-24 ${
                       item.status === "pending"
-                        ? "bg-yellow-500 text-white"
+                        ? "bg-teal-700 text-white"
                         : item.status === "approved"
                         ? "bg-green-500 text-white"
                         : "bg-red-500 text-white"
